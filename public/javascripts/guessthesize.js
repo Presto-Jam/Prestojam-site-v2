a57706cbf6ec2b06d090026f081aa696eeb67a5f
Prestopy <kingstontsai@gmail.com>
2024-01-05 19:24:31 +0800

    Added try again buttons when the guessthesize game ends

--- public/javascripts/guessthesize.js
+++ public/javascripts/guessthesize.js
@@ -16,23 +16,42 @@ document.getElementById("fullscr").addEventListener("click", function(event) {
     document.getElementById("fullscr").style.display = "none";
 });
 
-function warn(text, type) {
+function warn(text, type, buttonText, buttonAction) {
     let warningBox = document.getElementById("warning");
     let warningText = document.getElementById("warning-text");
     let fullScrBox = document.getElementById("fullscr");
     let fullScrText = document.getElementById("fullscr-text");
-
     if(type === "alert") {
         warningText.innerHTML = text;
         warningBox.style.display = "block";
 
+        let button = document.getElementById("warning-button")
+        if(buttonText != null) {
+            button.innerHTML = buttonText;
+            button.style.display = "block";
+        }
+
+        button.addEventListener("click", function(event) {
+            event.preventDefault();
+            if(buttonAction === "reset") {
+                wins = 0;
+                triesLeft = 3;
+                updateLevel();
+            }
+            button.style.display = "none";
+            warningBox.style.display = "none";
+        });
+
+        // gsap.fromTo(warningBox, {opacity: 0}, {opacity: 1, duration: 0.5});
+        // gsap.fromTo(warningBox, {y: -50}, {y: 0, duration: 0.5});
+
         setTimeout(function () {
             warningBox.style.display = "none";
+            button.style.display = "none";
         }, 5000);
     } else if(type === "success") {
         fullScrText.innerHTML = text;
         fullScrBox.style.display = "block";
-        fullScrBox.style.opacity = "0";
         fullScrBox.style.backgroundColor = "#22ff89";
 
         setTimeout(function () {
@@ -87,8 +106,7 @@ function updateLevel() {
             "Cheese", "Wow", "To be or not to be...", "...That is the question", "I'm a little teapot...", "...Or am I?"]
         GTStext.innerHTML = randomContent[Math.floor(Math.random() * randomContent.length)];
     } else {
-        alert("You have no more tries left :(. Better luck next time!");
-        //TODO: add a "play again" button to the context box
+        warn("You have no more tries left :(. Better luck next time!", "alert", "Play again", "reset");
     }
 }
 function generateSize() {
@@ -101,7 +119,7 @@ function generateSize() {
 }
 
 document.getElementById("GTSinput").addEventListener("keyup", function(event) {
-    if (event.keyCode === 13) {
+    if (event.key.toLowerCase() === "enter") {
         event.preventDefault();
         document.getElementById("GTSbutton").click();
     }
@@ -156,13 +174,11 @@ function validate() {
                 numbersGuessed.push(input);
             }
         } else {
-            gsap.to("#tries-text", {duration: 0.2, color: "#ff4e4e"});
-            gsap.to("#tries-text", {duration: 0.2, rotate: "+=20"});
-            gsap.to("#tries-text", {duration: 0.2, rotate: "-=40"});
-            gsap.to("#tries-text", {duration: 0.2, color: "#000000"});
+            warn("You have no more tries left :(. Better luck next time!", "alert", "Play again", "reset");
         }
     } else {
-        warn("Anti-cheat validation failed: Please close all side bars and try again.", "alert")
+        warn("Anti-cheat validation failed. Please close all side bars and try again. Your response was not counted" +
+            "towards your progress.", "alert")
 
         // TODO: fix this (PJM-28)
         clearInterval(detectDevToolsOpen());
