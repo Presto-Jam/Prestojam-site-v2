351409114d826060bfd743c50774f5e9e5af20c8
Prestopy <kingstontsai@gmail.com>
2024-01-06 10:55:15 +0800

    Added a timer to guess the size

--- public/javascripts/guessthesize.js
+++ public/javascripts/guessthesize.js
@@ -2,6 +2,9 @@ let triesLeft = 3;
 let wins = 0;
 let size
 let tryIncrement = 1;
+let level;
+
+let disabled = false;
 
 // Text size constraints
 let max = 100;
@@ -34,9 +37,8 @@ function warn(text, type, buttonText, buttonAction) {
         button.addEventListener("click", function(event) {
             event.preventDefault();
             if(buttonAction === "reset") {
-                wins = 0;
-                triesLeft = 3;
-                updateLevel();
+                updateLevel("reset");
+                updateLevel("update");
             }
             button.style.display = "none";
             warningBox.style.display = "none";
@@ -67,46 +69,97 @@ function warn(text, type, buttonText, buttonAction) {
         }, 3000);
     }
 }
-updateLevel();
-function updateLevel() {
-    if(triesLeft-1 > 0) {
-        let levelDiv = document.getElementById("level");
-        let levelText = document.getElementById("level-text");
-        let level = wins + 1;
+updateLevel("update");
 
-        // Focus on the input box & clear
-        document.getElementById("GTSinput").focus();
-        document.getElementById("GTSinput").value = "";
+let timerInterval;
+let defaultTime = 10;
+function setTimerInterval(variable) {
+    let timeLeft = variable+1;
 
-        // Clearing the numbers guessed list
-        numbersGuessed = [];
-
-        // Updating the try increment
-        tryIncrement = level - 1;
-        triesLeftText.innerHTML = "Tries left: " + triesLeft;
-
-        // Updating the multiple
-        if (wins < 10) {
-            multiple = 10 - wins;
-        } else {
-            multiple = 1;
+    let timerText = document.getElementById("timer-text");
+    timerInterval = setInterval(function () {
+        timeLeft--;
+        timerText.innerHTML = "Time left: " + timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            warn("You ran out of time! Better luck next time!", "alert", "Play again", "reset");
+            disabled = true;
         }
-        generateSize();
+        timerText.style.color = "rgb(" + ((defaultTime-timeLeft) * 255/defaultTime) + ", 0, 0)";
+    }, 1000);
+}
+function timer(action) {
+    let timeLeft;
+    if(action === "start") {
+        timeLeft = defaultTime;
+        setTimerInterval(timeLeft);
+    } else if(action === "reset") {
+        timeLeft = defaultTime;
+        clearInterval(timerInterval);
+        setTimerInterval(timeLeft)
+    } else if(action === "update") {
+        defaultTime += ((level-5)*2);
+        timeLeft = defaultTime;
+        clearInterval(timerInterval);
+        setTimerInterval(timeLeft);
+    } else if(action === "clear") {
+        clearInterval(timerInterval);
+        document.getElementById("timer-text").innerHTML = "";
+    }
+}
+function updateLevel(action) {
+    if(action === "update") {
+        level = wins + 1;
 
-        // Updating the level display
-        levelText.innerHTML = "Level " + level + " - mul. of " + multiple;
-        if (wins < 10) {
-            levelDiv.style.backgroundColor = "rgb(" + ((wins + 15) * 10) + ", 0, 0)";
-        } else {
-            levelDiv.style.backgroundColor = "rgb(255, 0, 0)";
+        if (level === 5) {
+            timer("start");
+        } else if(level > 5) {
+            timer("update");
         }
+        if (triesLeft - 1 > 0) {
+            let levelDiv = document.getElementById("level");
+            let levelText = document.getElementById("level-text");
 
-        // Updating the text contents
-        const randomContent = ["Interesting...", "Why, hello there!", "AAAHHHHH", "A wise man once said...",
-            "Cheese", "Wow", "To be or not to be...", "...That is the question", "I'm a little teapot...", "...Or am I?"]
-        GTStext.innerHTML = randomContent[Math.floor(Math.random() * randomContent.length)];
-    } else {
-        warn("You have no more tries left :(. Better luck next time!", "alert", "Play again", "reset");
+            // Focus on the input box & clear
+            document.getElementById("GTSinput").focus();
+            document.getElementById("GTSinput").value = "";
+
+            // Clearing the numbers guessed list
+            numbersGuessed = [];
+
+            // Updating the try increment
+            tryIncrement = level;
+            triesLeftText.innerHTML = "Tries left: " + triesLeft;
+
+            // Updating the multiple
+            if (wins < 10) {
+                multiple = 10 - wins;
+            } else {
+                multiple = 1;
+            }
+            generateSize();
+
+            // Updating the level display
+            levelText.innerHTML = "Level " + level + " - mul. of " + multiple;
+            if (wins < 10) {
+                levelDiv.style.backgroundColor = "rgb(" + ((wins + 15) * 10) + ", 0, 0)";
+            } else {
+                levelDiv.style.backgroundColor = "rgb(255, 0, 0)";
+            }
+
+            // Updating the text contents
+            const randomContent = ["Interesting...", "Why, hello there!", "AAAHHHHH", "A wise man once said...",
+                "Cheese", "Wow", "To be or not to be...", "...That is the question", "I'm a little teapot...", "...Or am I?"]
+            GTStext.innerHTML = randomContent[Math.floor(Math.random() * randomContent.length)];
+        } else {
+            warn("You have no more tries left :(. Better luck next time!", "alert", "Play again", "reset");
+            disabled = true;
+        }
+    } else if(action === "reset") {
+        wins = 0;
+        triesLeft = 3;
+        level = 1;
+        timer("clear");
     }
 }
 function generateSize() {
@@ -139,41 +192,45 @@ function detectDevToolsOpen() {
 
 setInterval(detectDevToolsOpen, 1000);
 
+// LEVEL VALIDATION -----------------------------------------------------------------------------------
 function validate() {
+    if(triesLeft === 0) {disabled = true;}
+
     let input = parseInt(document.getElementById("GTSinput").value);
 
     // Pre-checks
-    if(input > 100) {
-        warn("Number exceeds limit. Please enter a number less than 100", "alert");
-        return;
-    }
-    if(input % multiple !== 0) {
-        warn("Number doesn't match constraints. Please enter a multiple of " + multiple, "alert");
-        return;
-    }
-    if(numbersGuessed.includes(input)) {
-        warn("Duplicate guess. Please enter a number you haven't tried", "alert");
-        return;
+    if(disabled !== true) {
+        if (input > 100) {
+            warn("Number exceeds limit. Please enter a number less than 100", "alert");
+            return;
+        }
+        if (input % multiple !== 0) {
+            warn("Number doesn't match constraints. Please enter a multiple of " + multiple, "alert");
+            return;
+        }
+        if (numbersGuessed.includes(input)) {
+            warn("Duplicate guess. Please enter a number you haven't tried", "alert");
+            return;
+        }
     }
 
     if(cheat === false) {
-        if (triesLeft !== 0) {
+        if (triesLeft !== 0 || disabled !== true) {
             if (input === size) {
                 warn("Correct!", "success");
                 wins++;
                 triesLeft += tryIncrement;
-                updateLevel();
+                updateLevel("update");
             } else {
                 warn(`Incorrect... Try again. You have ${triesLeft-1} tries left`, "fail");
 
                 triesLeft--;
                 triesLeftText.innerHTML = "Tries left: " + triesLeft;
-                console.log(size);
-                console.log(input);
 
                 numbersGuessed.push(input);
+                timer("reset");
             }
-        } else {
+        } else if(triesLeft <= 0) {
             warn("You have no more tries left :(. Better luck next time!", "alert", "Play again", "reset");
         }
     } else {
@@ -191,4 +248,9 @@ function validate() {
         generateSize();
         cheat = false;
     }
+}
+function debugmode() {
+    wins++;
+    triesLeft += tryIncrement;
+    updateLevel("update");
 }
