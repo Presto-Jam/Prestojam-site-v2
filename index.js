c9d406d9ddbd1f0d76ba03b19d13a79c6b7732c5
Prestopy <kingstontsai@gmail.com>
2024-01-07 20:56:33 +0800

    Got index.js to work. Some modifications needed to load pages other than index.html but thanks to decodedFinn!

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
+// ---- CREDITS ----
+// thx to DecodedFinn for this code :D
+// I modified the code for loading HTML to load all HTML files instead of only index.html
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const port = 3000; // Set the port number you want to use
 
-// Define a route to serve the HTML file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+// HTML files
+app.get('/views/:file', (req, res) => {
+    var file = req.params["file"]
+    res.sendFile(path.join(__dirname, `views/${file}`));
+});
+app.get('/views/archive/:file', (req, res) => {
+    var file = req.params["file"]
+    res.sendFile(path.join(__dirname, `views/archive/${file}`));
 });
 
+// JS files
 app.get('/javascripts/:file', (req,res) => {
     var file = req.params["file"]
     res.sendFile(path.join(__dirname, `src/javascripts/${file}`))
 })
+app.get('/javascripts/archive/:file', (req,res) => {
+    var file = req.params["file"]
+    res.sendFile(path.join(__dirname, `src/javascripts/${file}`))
+})
 
+// CSS files
 app.get('/stylesheets/:file', (req,res) => {
     var file = req.params["file"]
     res.sendFile(path.join(__dirname, `src/stylesheets/${file}`))
 })
+app.get('/stylesheets/archive/:file', (req,res) => {
+    var file = req.params["file"]
+    res.sendFile(path.join(__dirname, `src/stylesheets/${file}`))
+})
+
 
 // Start the server
 app.listen(port, () => {
