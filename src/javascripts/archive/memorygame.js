c2b18202f303631066f3fe61721f5786f8d1ec46
Prestopy <kingstontsai@gmail.com>
2024-01-09 18:09:23 +0800

    Finished the memory game

--- src/javascripts/archive/memorygame.js
+++ src/javascripts/archive/memorygame.js
@@ -1,59 +1,118 @@
 let gameContainer = document.getElementById("game");
-let size = 4;
-let totalCards = size*size;
+let size = 0;
+let totalCards = 0;
 
 let cardContents = [];
 let cardStatuses = [];
 
-function updateLevel() {
-    // generate cards
-    for(let rows=0;rows<size; rows++) {
-        let cardRow = document.createElement("div");
-        cardRow.classList.add("card-row");
-        cardRow.setAttribute("id", `card-row-${rows}`);
-        gameContainer.appendChild(cardRow);
+let flipTime = 2000;
 
-        for(let cols=0;cols<size; cols++) {
-            let card = document.createElement("div");
-            cardRow.appendChild(card);
-            card.classList.add("card");
-            card.setAttribute("id", `card-${rows}-${cols}`)
+let level = 0;
+let triesLeft = 0;
+let timeLeft = 0;
 
-            let cardText = document.createElement("p");
-            card.appendChild(cardText);
-            cardText.classList.add("card-text");
-            cardText.setAttribute("id", `card-text-${rows}-${cols}`);
-            cardText.innerHTML = "?";
+function setTimerInterval() {
+	let timerText = document.getElementById("timer-text");
+	timerInterval = setInterval(function () {
+		timeLeft--;
 
-            // on-click
-            card.addEventListener("click", function(event) {
-                event.preventDefault();
-
-                cardClicked(event.target.id);
-            });
-
-            // fill card-statuses array
-            cardStatuses.push(false);
-        }
-    }
-
-    // generate card contents
-    let numbers = [];
-    let possibleCombs = [];
-    for(let i=1;i<(totalCards/2)+1;i++) {
-        possibleCombs.push(i); possibleCombs.push(i);
-    }
-    for(let i=0; i<possibleCombs.length; i++) {
-        numbers.push(possibleCombs[i]);
-    }
-
-    for(let i=0;i<totalCards; i++) {
-        let random = Math.floor(Math.random() * numbers.length);
-        cardContents.push(numbers[random]);
-        numbers.splice(random, 1);
-    }
+		timerText.innerHTML = "Time left: " + timeLeft;
+		if (timeLeft <= 0) {
+			clearInterval(timerInterval);
+			warn("You ran out of time! Better luck next time!", "alert", "Play again", "reset");
+			disabled = true;
+		}
+		timerText.style.color = "rgb(" + ((defaultTime-timeLeft) * 255/defaultTime) + ", 0, 0)";
+	}, 1000);
 }
-updateLevel();
+
+function updateTimer(action) {
+	if(action === "start") {
+		setTimerInterval();
+	} else if(action === "reset") {
+		clearInterval(timerInterval);
+		setTimerInterval(timeLeft)
+	} else if(action === "clear") {
+		clearInterval(timerInterval);
+		document.getElementById("timer-text").innerHTML = "";
+	}
+}
+function updateLevel(type) {
+    // generate cards
+	if(type === "update") {
+		level++;
+
+		if(level > 7) {updateTimer("clear");}
+
+		if(level <= 6) {
+			size = level * 2 // MAX: 6x6
+			triesLeft = size*size + level*2; // MAX BONUS: 30
+		} else if(level >= 7) {
+			triesLeft = (size*size/2) + 20; // MAX BONUS: 20
+			timeLeft = level*10; // MIN: 70
+			updateTimer("start");
+		}
+		totalCards = size*size;
+
+		document.getElementById("tries-text").innerHTML = `Tries left: ${triesLeft}`;
+		document.getElementById("level-text").innerHTML = `Level ${level}`;
+
+		// clear game
+		document.getElementById("game").innerHTML = "";
+		cardStatuses = [];
+		cardContents = [];
+
+		for (let rows = 0; rows < size; rows++) {
+			let cardRow = document.createElement("div");
+			cardRow.classList.add("card-row");
+			cardRow.setAttribute("id", `card-row-${rows}`);
+			gameContainer.appendChild(cardRow);
+
+			for (let cols = 0; cols < size; cols++) {
+				let card = document.createElement("div");
+				cardRow.appendChild(card);
+				card.classList.add("card");
+				card.setAttribute("id", `card-${rows}-${cols}`)
+
+				let cardText = document.createElement("p");
+				card.appendChild(cardText);
+				cardText.classList.add("card-text");
+				cardText.setAttribute("id", `card-text-${rows}-${cols}`);
+				cardText.innerHTML = "?";
+
+				// on-click
+				card.addEventListener("click", function (event) {
+					event.preventDefault();
+
+					cardClicked(event.target.id);
+				});
+
+				// fill card-statuses array
+				cardStatuses.push(false);
+			}
+		}
+
+		// generate card contents
+		let numbers = [];
+		let possibleCombs = [];
+		for (let i = 1; i < (totalCards / 2) + 1; i++) {
+			possibleCombs.push(i);
+			possibleCombs.push(i);
+		}
+		for (let i = 0; i < possibleCombs.length; i++) {
+			numbers.push(possibleCombs[i]);
+		}
+
+		for (let i = 0; i < totalCards; i++) {
+			let random = Math.floor(Math.random() * numbers.length);
+			cardContents.push(numbers[random]);
+			numbers.splice(random, 1);
+		}
+	} else if(type === "reset") {
+		level = 0;
+	}
+}
+updateLevel("update");
 function warn(text, type, buttonText, buttonAction) {
     let warningBox = document.getElementById("warning");
     let warningText = document.getElementById("warning-text");
@@ -79,13 +138,10 @@ function warn(text, type, buttonText, buttonAction) {
             warningBox.style.display = "none";
         });
 
-        // gsap.fromTo(warningBox, {opacity: 0}, {opacity: 1, duration: 0.5});
-        // gsap.fromTo(warningBox, {y: -50}, {y: 0, duration: 0.5});
-
         setTimeout(function () {
             warningBox.style.display = "none";
             button.style.display = "none";
-        }, 5000);
+        }, flipTime);
     } else if(type === "success") {
         successBox.style.display = "block";
         successText.innerHTML = text;
@@ -122,6 +178,9 @@ let card1 = {
     numPos: null
 };
 
+/**
+ * @param {string} rc
+ */
 function flipCard(rc) {
     let row = parseInt(rc.substring(0,1));
     let col = parseInt(rc.substring(2,3));
@@ -130,10 +189,14 @@ function flipCard(rc) {
     let targetText = document.getElementById(`card-text-${rc}`);
     if(!cardStatuses[pos]) {
         // card is not flipped; flip over
-        targetText.innerHTML = currCard.contents;
+        targetText.innerHTML = cardContents[pos];
+        targetText.style.fontSize = "50px";
+        cardStatuses[pos] = true;
     } else {
         // card is flipped; flip back
         targetText.innerHTML = "?";
+        cardStatuses[pos] = false;
+        targetText.style.fontSize = "20px";
     }
 }
 function cardClicked(cardClicked) {
@@ -157,21 +220,29 @@ function cardClicked(cardClicked) {
     currCard.numPos = currNum;
 
     cardsFlipped++;
-    cardStatuses[currCard.numPos] = true;
-
-    // update card
     flipCard(currCard.rc);
 
     if(cardsFlipped === 1) {
         // first card
         Object.assign(card1, currCard);
     } else if(cardsFlipped === 2) {
+		triesLeft--;
+		document.getElementById("tries-text").innerHTML = `Tries left: ${triesLeft}`
+		if(triesLeft <= 0) {
+			warn("You ran out of tries. Better luck next time!", "alert", "Try again", "reset");
+		}
+
+
         // check if match
         if(card1.contents === currCard.contents) {
             // match
             warn("Match!", "success");
             cardStatuses[card1.numPos] = true;
             cardStatuses[currCard.numPos] = true;
+
+            document.getElementById(`card-text-${card1.rc}`).style.color = "#00ff00";
+            document.getElementById(`card-text-${currCard.rc}`).style.color = "#00ff00";
+            reset();
         } else {
             // no match
             warn("No match!", "fail");
@@ -187,17 +258,26 @@ function cardClicked(cardClicked) {
                 targetText2.innerHTML = "";
 
                 // reset card statusesno
-                cardStatuses[card1.numPos] = false;
-                cardStatuses[currCard.numPos] = false;
-            }, 2000);
+                flipCard(card1.rc);
+                flipCard(currCard.rc);
+
+                reset();
+            }, flipTime);
         }
 
 
         // reset
-        setTimeout(function() {
+        function reset() {
+            console.log("reset");
             currCard = {};
             card1 = {};
             cardsFlipped = 0;
-        }, 2010);
+        }
+
+
+		// check if all card statuses are true
+		if(cardStatuses.includes(false) === false) {
+			updateLevel("update");
+		}
     }
 }
